eb36c3c8416537bcc10bcba52ba524ad549539af
david <david_Alisson60@yahoo.com.br>
2025-07-02 21:20:13 -0300

    S.1 - 3 - Gerar o projeto com Spring

---
+++ src/test/java/com/mbalem/demo_spring_rev_jpa/DemoSpringRevJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbalem.demo_spring_rev_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringRevJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
