901387c2cfdc656589ade4194d6795b6dc805dd7
david <david_Alisson60@yahoo.com.br>
2025-07-10 18:37:51 -0300

    S1 - 5 - Mapear a tabela de autores com JPA

---
+++ src/main/java/com/mbalem/demo_spring_rev_jpa/domain/Autor.java
@@ -0,0 +1,65 @@
+package com.mbalem.demo_spring_rev_jpa.domain;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "autores")
+public class Autor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id_autor", nullable = false)
+    private Long id;
+
+    @Column(name = "nome", length = 45, nullable = false)
+    private String nome;
+
+    @Column(name = "sobrenome", length = 45, nullable = false)
+    private String sobrenome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Autor autor = (Autor) o;
+        return Objects.equals(id, autor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public Autor(Long id, String nome, String sobrenome) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
