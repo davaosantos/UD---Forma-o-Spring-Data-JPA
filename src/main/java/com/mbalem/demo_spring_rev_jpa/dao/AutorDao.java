e3db0f5d3d4035f553595b075653a88af13d8e57
david <david_Alisson60@yahoo.com.br>
2025-07-16 20:12:15 -0300

    S1 - 8/9 Salvar um autor e config da transação

--- src/main/java/com/mbalem/demo_spring_rev_jpa/dao/AutorDao.java
+++ src/main/java/com/mbalem/demo_spring_rev_jpa/dao/AutorDao.java
@@ -5,6 +5,7 @@ import com.mbalem.demo_spring_rev_jpa.domain.Autor;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class AutorDao {
@@ -12,6 +13,7 @@ public class AutorDao {
     @PersistenceContext
     private EntityManager manager;
 
+    @Transactional
     public void save(Autor autor){
         this.manager.persist(autor);
     }
