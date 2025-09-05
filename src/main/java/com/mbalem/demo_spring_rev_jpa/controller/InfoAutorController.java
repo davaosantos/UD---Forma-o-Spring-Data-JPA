3621e139079c4602ef2d908d512873525147deee
david <david_Alisson60@yahoo.com.br>
2025-09-04 22:14:45 -0300

    S6 - 52 -> Busca por cargo

--- src/main/java/com/mbalem/demo_spring_rev_jpa/controller/InfoAutorController.java
+++ src/main/java/com/mbalem/demo_spring_rev_jpa/controller/InfoAutorController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("info")
 public class InfoAutorController {
@@ -19,4 +21,10 @@ public class InfoAutorController {
     private InfoAutor getInfoAutorById(@PathVariable("id") Long idAutor){
         return infoAutorService.findById(idAutor);
     }
+
+    @GetMapping("cargo/{cargo}")
+    private List<InfoAutor> getContainsCargo(@PathVariable("cargo") String cargo){
+        return infoAutorService.findAllContainsCargo(cargo);
+    }
+
 }
--- src/main/java/com/mbalem/demo_spring_rev_jpa/service/InfoAutorService.java
+++ src/main/java/com/mbalem/demo_spring_rev_jpa/service/InfoAutorService.java
@@ -4,8 +4,11 @@ import com.mbalem.demo_spring_rev_jpa.domain.InfoAutor;
 import com.mbalem.demo_spring_rev_jpa.repository.InfoAutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true) //Apenas métodos de transação com consulta, para todos os metodos da classe
@@ -18,5 +21,14 @@ public class InfoAutorService {
         InfoAutor infoAutor = new InfoAutor();
         infoAutor.setIdInfo(infoAutorId);
         return this.infoAutorRepository.findOne(Example.of(infoAutor)).orElseGet(InfoAutor::new);
+    }
+
+    //Busca pelo termo cargo em qualquer parte do nome do cargo
+    public List<InfoAutor> findAllContainsCargo(String cargo){
+        InfoAutor infoAutor = new InfoAutor();
+        infoAutor.setCargo(cargo);
+        ExampleMatcher cargoMatcher = ExampleMatcher.matching()
+                .withMatcher("cargo", ExampleMatcher.GenericPropertyMatchers.contains());
+        return this.infoAutorRepository.findAll(Example.of(infoAutor, cargoMatcher));
     }
 }
