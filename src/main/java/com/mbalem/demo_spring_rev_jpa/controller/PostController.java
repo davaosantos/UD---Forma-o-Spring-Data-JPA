22614917e8b7b0629cf872892ab22289ff6b95a5
david <david_Alisson60@yahoo.com.br>
2025-10-06 22:24:27 -0300

    S8 - 71 - Inserir posts com Autor e categorias relacionadas

--- src/main/java/com/mbalem/demo_spring_rev_jpa/controller/PostController.java
+++ src/main/java/com/mbalem/demo_spring_rev_jpa/controller/PostController.java
@@ -1,8 +1,11 @@
 package com.mbalem.demo_spring_rev_jpa.controller;
 
 
+import com.mbalem.demo_spring_rev_jpa.domain.Post;
 import com.mbalem.demo_spring_rev_jpa.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,10 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @PostMapping
+    public Post save(@RequestBody Post post){
+        return this.postService.save(post);
+    }
 
 }
--- src/main/java/com/mbalem/demo_spring_rev_jpa/service/PostService.java
+++ src/main/java/com/mbalem/demo_spring_rev_jpa/service/PostService.java
@@ -1,15 +1,49 @@
 package com.mbalem.demo_spring_rev_jpa.service;
 
 
+import com.mbalem.demo_spring_rev_jpa.domain.Autor;
+import com.mbalem.demo_spring_rev_jpa.domain.Categoria;
+import com.mbalem.demo_spring_rev_jpa.domain.Post;
 import com.mbalem.demo_spring_rev_jpa.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository postRepository;
+
+    @Autowired
+    private AutorService autorService;
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @Transactional
+    public Post save(Post post){
+
+        Autor autor = this.autorService.findById(post.getAutor().getId());
+        post.setAutor(autor);
+
+        // busco as categorias conforme o titulo de String, para ter os objetos categorias em estado persistente
+        List<String> titulos = post.getCategorias().stream().map(
+                Categoria::getTitulo
+        ).toList();
+
+        List<Categoria> categorias = this.categoriaService.findByTitulos(titulos); // Estado persistente
+
+        if (!categorias.isEmpty()){
+            post.setCategorias(categorias);
+        }
+
+        return this.postRepository.save(post);
+    }
 
 
 }
